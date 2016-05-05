06843b9900a84f8591778db8a293d7f1ca68b91f
Vlad Fratila <vlad.fratila@cti.pub.ro>
2016-05-05 23:37:06 +0300

    Implemented Clock Observable using TimerTask

---
+++ src/refrigerator/Clock.java
@@ -0,0 +1,45 @@
+package refrigerator;
+
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by vlad on 05/05/16.
+ */
+public class Clock extends Observable {
+    // minimum interval in which the state changes in MILLISECONDS -- defaults to 1 minute
+    // change this for debug purposes
+    public static final long TICK_INTERVAL = 1000;
+    private Timer tickTimer = new Timer();
+    private static Clock instance;
+
+    public enum Events {
+        CLOCK_TICKED_EVENT
+    };
+
+    /**
+     * Schedule regular ticks
+     */
+    private Clock() {
+        tickTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                setChanged();
+                notifyObservers(Events.CLOCK_TICKED_EVENT);
+            }
+        }, TICK_INTERVAL, TICK_INTERVAL);
+    }
+
+    /**
+     * Get the instance
+     *
+     * @return returns the Clock
+     */
+    public static Clock instance() {
+        if (instance == null) {
+            instance = new Clock();
+        }
+        return instance;
+    }
+}
